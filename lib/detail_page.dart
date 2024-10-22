1701f811dcdcede5d48df867006f5abcd12a96a9
jonyxz <johanridho08@gmail.com>
2024-10-22 14:12:59 +0700

    fix home page

---
+++ lib/detail_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:listkuliner/makanan.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key, required Makanan makanan});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:listkuliner/list_item.dart';
 import 'package:listkuliner/styles.dart';
+import 'package:listkuliner/makanan.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,64 +11,28 @@ class HomePage extends StatelessWidget {
     return ListView(
       padding: const EdgeInsets.all(10),
       children: [
-        const SizedBox(height: 20),
-        const Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Icon(Icons.list_alt_sharp, size: 30),
-          SizedBox(width: 10),
-          Text('List Kuliner', style: textHeader1),
-        ]),
-        const Padding(padding: EdgeInsets.only(bottom: 20)),
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 8),
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromARGB(255, 178, 178, 178),
-                offset: Offset(1.0, 2.0),
-                blurRadius: 6.0,
-              ),
-            ],
-          ),
-          height: 100,
-          padding: const EdgeInsets.symmetric(
-            horizontal: 15,
-            vertical: 10,
-          ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 85,
-                height: 75,
-                color: Colors.grey,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Title",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    "Sub Title",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
-        ListItem(),
+        Column(
+          children: [
+            const SizedBox(height: 20),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.list_alt_sharp, size: 30),
+                SizedBox(width: 10),
+                Text('List Kuliner', style: textHeader1),
+              ],
+            ),
+            const Padding(padding: EdgeInsets.only(bottom: 20)),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: listMakanan.length,
+              padding: const EdgeInsets.all(10),
+              itemBuilder: (context, index) {
+                return ListItem(makanan: listMakanan[index]);
+              },
+            ),
+          ],
+        )
       ],
     );
   }
